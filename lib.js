013a21f8b58ad11ae7ab923cc669122989e589b7
Peter Barraud <barraud@adobe.com>
2020-07-18 09:43:52 +0530

    some formatting changes

--- lib.js
+++ lib.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
     $.getJSON('data.json', function(data) {
+        $( '.wait' ).hide();
+        $( '.done-waiting' ).show();
         console.log(data);
     });
 });

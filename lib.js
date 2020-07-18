8fc53691dd5f927995fdf27a31dbbd2a8c4ab301
Peter Barraud <barraud@adobe.com>
2020-07-18 10:12:13 +0530

    first bit of real data

--- lib.js
+++ lib.js
@@ -1,7 +1,21 @@
 $( document ).ready(function() {
+    var chemistrydata = null;
+    var cursor_pos = 0;
     $.getJSON('data.json', function(data) {
+        chemistrydata = data;
         $( '.wait' ).hide();
+        // https://stackoverflow.com/a/31997858
+        $( "#cursor_position" ).val(cursor_pos).trigger("change");
         $( '.done-waiting' ).show();
-        console.log(data);
     });
+
+
+
+    $( "#cursor_position" ).change(function() {
+        $( '#name').text(chemistrydata[cursor_pos].name);
+        $( '#symbol').text(chemistrydata[cursor_pos].symbol);
+        $( '#valency').text('Valency: ' + chemistrydata[cursor_pos].valency);
+        $( '#atomicnumber').text('Atomic number: ' + chemistrydata[cursor_pos].atomicnumber);
+      });
+
 });
